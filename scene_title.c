5011b33798436cd564544dd5323c9713838b176f
Jared De Blander <jared0x90@gmail.com>
2026-02-03 14:32:01 -0500

    jack games added

--- scene_title.c
+++ scene_title.c
@@ -4,10 +4,13 @@
 #define TITLE_ROW 8
 #define PROMPT_ROW 14
 
+#define COPYRIGHT_ROW 20
+
 void scene_title_enter(void) {
     // Clear any previous text and display title
     consoleDrawText(11, TITLE_ROW, "STARSHMUP");
     consoleDrawText(10, PROMPT_ROW, "PRESS START");
+    consoleDrawText(6, COPYRIGHT_ROW, "(C) 2026 JACK GAMES");
 }
 
 Scene scene_title_update(u16 pad) {
@@ -15,6 +18,7 @@ Scene scene_title_update(u16 pad) {
         // Clear title text before transitioning
         consoleDrawText(11, TITLE_ROW, "         ");
         consoleDrawText(10, PROMPT_ROW, "           ");
+        consoleDrawText(6, COPYRIGHT_ROW, "                   ");
         return SCENE_GAMEPLAY;
     }
     return SCENE_TITLE;
