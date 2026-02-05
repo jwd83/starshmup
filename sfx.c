8d42e16929d617aa3f3348468dbbd3bcd1126b42
Jared De Blander <jared0x90@gmail.com>
2026-02-04 20:57:39 -0500

    sfx updates

--- sfx.c
+++ sfx.c
@@ -9,6 +9,9 @@ extern char sfx_explosion, sfx_explosion_end;
 static brrsamples g_sfxLaser;
 static brrsamples g_sfxExplosion;
 
+static u8 g_sfxLockFrames = 0;
+static u8 g_laserCooldownFrames = 0;
+
 enum {
     // spcPlaySound() index 0 is the last sound registered via spcSetSoundEntry().
     SFX_IDX_LASER = 0,
@@ -32,35 +35,56 @@ void sfx_init(void) {
     laser_len = (u16)(&sfx_laser_end - &sfx_laser);
 
     // Register EXPLOSION first so LASER becomes index 0 (most frequently played).
-    spcSetSoundEntry(15, 8, 2, explosion_len, (u8 *)&sfx_explosion, &g_sfxExplosion);
-    spcSetSoundEntry(12, 8, 6, laser_len, (u8 *)&sfx_laser, &g_sfxLaser);
+    spcSetSoundEntry(15, 8, 3, explosion_len, (u8 *)&sfx_explosion, &g_sfxExplosion);
+    spcSetSoundEntry(15, 8, 4, laser_len, (u8 *)&sfx_laser, &g_sfxLaser);
 }
 
 void sfx_process(void) {
+    if (g_sfxLockFrames) {
+        g_sfxLockFrames--;
+    }
+    if (g_laserCooldownFrames) {
+        g_laserCooldownFrames--;
+    }
     spcProcess();
 }
 
+static u8 sfx_can_play_laser(void) {
+    return (g_sfxLockFrames == 0) && (g_laserCooldownFrames == 0);
+}
+
 void sfx_ui_confirm(void) {
-    spcPlaySoundV(SFX_IDX_LASER, 10);
+    if (!sfx_can_play_laser()) return;
+    g_laserCooldownFrames = 10;
+    spcPlaySoundV(SFX_IDX_LASER, 15);
 }
 
 void sfx_shot(void) {
-    spcPlaySoundV(SFX_IDX_LASER, 8);
+    if (!sfx_can_play_laser()) return;
+    g_laserCooldownFrames = 8;
+    spcPlaySoundV(SFX_IDX_LASER, 15);
 }
 
 void sfx_enemy_hit(void) {
-    spcPlaySoundV(SFX_IDX_LASER, 6);
+    if (!sfx_can_play_laser()) return;
+    g_laserCooldownFrames = 10;
+    spcPlaySoundV(SFX_IDX_LASER, 12);
 }
 
 void sfx_enemy_down(void) {
+    g_sfxLockFrames = 24;
+    g_laserCooldownFrames = 24;
     spcPlaySoundV(SFX_IDX_EXPLOSION, 15);
 }
 
 void sfx_level_up(void) {
+    g_sfxLockFrames = 28;
+    g_laserCooldownFrames = 28;
     spcPlaySoundV(SFX_IDX_EXPLOSION, 12);
 }
 
 void sfx_player_down(void) {
+    g_sfxLockFrames = 60;
+    g_laserCooldownFrames = 60;
     spcPlaySoundV(SFX_IDX_EXPLOSION, 15);
 }
-
